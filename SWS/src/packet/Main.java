f45b0aea22dddb30e2da1be0a7e0b95774fcfc70
Jovo <Jovo@JOVO96>
2017-04-14 19:38:29 +0200

    Jovo commited.

--- SWS/src/packet/Main.java
+++ SWS/src/packet/Main.java
@@ -3,7 +3,7 @@ package packet;
 import view.MyWindow;
 
 public class Main {
-
+	//Hello collaborators!
 	public static void main(String[] args) {
 		MyWindow myWindow = new MyWindow();
 		myWindow.setVisible(true);
